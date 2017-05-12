04f3b4e3f398794e55e0e08e66baa712d76f8966
mh-cbon <mh-cbon@users.noreply.github.com>
2017-05-12 13:39:41 +0200

    refactoring

--- test.sh
+++ test.sh
@@ -5,18 +5,29 @@ set -ex
 rm `which lister`
 go install
 
+
+lister string:StringSlice | grep "Empty(" || exit 1;
+
 rm -fr gen_test
 lister string:gen_test/StringSlice || exit 1;
 ls -al gen_test | grep "stringslice.go" || exit 1;
 cat gen_test/stringslice.go | grep "Empty(" || exit 1;
+rm -fr gen_test
 
 rm -fr demo/gen
 go generate demo/lib.go
-go test
 ls -al demo | grep "gen" || exit 1;
 ls -al demo/gen | grep "tomates.go" || exit 1;
+# rm -fr demo/gen # keep it for demo
 
-rm -fr gen_test
+rm -fr demo/gen
+go generate github.com/mh-cbon/lister/demo
+ls -al demo | grep "gen" || exit 1;
+ls -al demo/gen | grep "tomates.go" || exit 1;
+# rm -fr demo/gen # keep it for demo
+
+go test
+
 
 echo ""
 echo "ALL GOOD!"
